99c00e09eecbb615fc54692571fc2da29305ac28
Utsav Shrestha <mkutsav@gmail.com>
2025-05-16 09:33:20 +0545

    project initiallization

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('hello' ,function (Request $request){
+    return'test hello';
+
+
+}
+);
